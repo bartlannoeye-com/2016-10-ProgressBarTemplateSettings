bb3dc680a0e92a3c1aa669aad3f88cd2d23cca2f
bartlannoeye <bart.lannoeye@gmail.com>
2016-10-04 22:46:30 +0200

    Added five different progress bars :)

---
+++ Src/ProgressBar/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace ProgressBar
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            var diameter = ProgressBar.TemplateSettings.EllipseDiameter;
+        }
+    }
+}
---
+++ Src/ProgressBar/MyProgressBar.cs
@@ -0,0 +1,47 @@
+﻿using Windows.UI.Xaml;
+
+namespace ProgressBar
+{
+    public sealed class MyProgressBar : Windows.UI.Xaml.Controls.ProgressBar
+    {
+        public MyProgressBar()
+        {
+            this.DefaultStyleKey = typeof (Windows.UI.Xaml.Controls.ProgressBar);
+            this.Loaded += (sender, args) => TemplateSettings = new MyProgressBarTemplateSettings(this);
+        }
+
+        public static readonly DependencyProperty TemplateSettingsProperty = DependencyProperty.Register(
+            "TemplateSettings", typeof (MyProgressBarTemplateSettings), typeof (MyProgressBar),
+            new PropertyMetadata(default(MyProgressBarTemplateSettings)));
+
+        public new MyProgressBarTemplateSettings TemplateSettings
+        {
+            get { return (MyProgressBarTemplateSettings) GetValue(TemplateSettingsProperty); }
+            set { SetValue(TemplateSettingsProperty, value); }
+        }
+    }
+
+    public class MyProgressBarTemplateSettings : DependencyObject
+    {
+        public MyProgressBarTemplateSettings()
+        {
+        }
+
+        public MyProgressBarTemplateSettings(MyProgressBar progressBar)
+        {
+            ContainerAnimationStartPosition = -1*progressBar.ActualWidth / 10;
+            ContainerAnimationEndPosition = progressBar.ActualWidth * 3/8;
+            EllipseAnimationEndPosition = progressBar.ActualWidth * 2/3;
+            EllipseAnimationWellPosition = progressBar.ActualWidth / 3;
+            EllipseDiameter = 12;
+        }
+
+        public double ContainerAnimationEndPosition { get; set; }
+        public double ContainerAnimationStartPosition { get; set; }
+        public double EllipseAnimationEndPosition { get; set; }
+        public double EllipseAnimationWellPosition { get; set; }
+        public double EllipseDiameter { get; set; }
+        public double EllipseOffset { get; set; }
+        public double IndicatorLengthDelta { get; set; }
+    }
+}
